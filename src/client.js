3f3bd73584d9b24d8deedf5f12fc9e577589e1d0
Samuel Ferracini <samferracini@gmail.com>
2022-09-14 22:28:33 -0300

    feat: rename core to client

---
+++ src/client.js
@@ -0,0 +1,95 @@
+import dayjs from "dayjs";
+
+import Discord from "discord.js";
+
+import { timeout } from "./utils.js";
+import Messenger from "./messenger.js";
+
+class Client {
+  constructor() {
+    this.client = new Discord.Client({
+      intents: ["GuildVoiceStates", "GuildMessages", "Guilds"],
+    });
+  }
+
+  setup() {
+    this.targetUsers = process.env.MISSING_USERS_IDS.split(",").map((id) => ({
+      id,
+      lastActivity: null,
+    }));
+
+    this.client.login(process.env.CLIENT_TOKEN);
+
+    this.client.on("ready", () => {
+      console.log(`Logged in as ${this.client.user.tag}!`);
+    });
+
+    this.client.on("voiceStateUpdate", (_, newState) => {
+      this.updateUsersAcitivity(newState);
+    });
+  }
+
+  updateUsersAcitivity(newState) {
+    if (newState.member.user.bot) return;
+
+    const { user } = newState.member;
+
+    const idx = this.targetUsers.findIndex((tu) => tu.id === user.id);
+
+    if (idx === -1) return console.log("User not found");
+
+    Object.assign(this.targetUsers[idx], {
+      lastActivity: new Date(),
+    });
+  }
+
+  getMainChannel() {
+    const channel = this.client.channels.cache.find(
+      ({ id }) => id === process.env.MAIN_CHANNEL_ID
+    );
+
+    if (!channel) throw new Error("Main channel not found");
+
+    return channel;
+  }
+
+  sendMissingMessage(user) {
+    const channel = this.getMainChannel();
+
+    Messenger.sendMessage(user, channel);
+  }
+
+  hasExpired(date) {
+    if (!date) return true;
+
+    const timeToExpire = dayjs(date).add(12, "hours");
+
+    const now = new Date();
+
+    const hasExpired = !dayjs(now).isBefore(timeToExpire);
+
+    return hasExpired;
+  }
+
+  async checkUsersActivity() {
+    const inactiveUsers = this.targetUsers.filter((e) =>
+      this.hasExpired(e.lastActivity)
+    );
+
+    await Promise.all(
+      inactiveUsers.map(async (u) => {
+        const discordUser = await this.client.users.fetch(u.id);
+
+        this.sendMissingMessage(discordUser);
+      })
+    );
+  }
+
+  async execute() {
+    setInterval(() => {
+      this.checkUsersActivity();
+    }, +process.env.LOOP_INTERVAL || 12 * 60 * 60);
+  }
+}
+
+export default new Client();
